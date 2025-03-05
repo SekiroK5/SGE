68c8c71ffc71cedaf948d066e3c1dc859414f412
SekiroK5 <arin01246@gmail.com>
2025-03-04 22:37:53 -0600

    check: agregar login y registro

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,10 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+//Rutas 
+const authRoutes = require('./routes/auth.js'); //importa las rutas de auth
+
 const app = express();
 
+//Midlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Rutas
+app.use('/auth',authRoutes);// Monta las rutas bajo el prefijo de  '/auth'
+
+//Mensaje que confirma las rutas
+console.log("Rutas de autenticación cargadas en /auth");
 
 module.exports = app;
