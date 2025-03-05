e3f26baff95567f8e552f183acc27afbde98ec34
SekiroK5 <arin01246@gmail.com>
2025-03-05 16:00:27 -0600

    Acomodo de frontend

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component'; // Ajusta esta ruta según tu estructura de carpetas
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta predeterminada
+  { path: 'home', component: HomeComponent },
+  // Aquí puedes agregar otras rutas según necesites
+  { path: '**', redirectTo: '' } // Redirección para rutas no encontradas
+];
