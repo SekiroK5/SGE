42323e4c80ada9df1e0533e017670b35e8ba7003
SekiroK5 <arin01246@gmail.com>
2025-03-09 14:29:37 -0600

    Correccion de las carpetas y mejora del frontend

--- BACKEND/FRONTEND/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home/home.component'; // Ajusta esta ruta según tu estructura de carpetas
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Ruta predeterminada
-  { path: 'home', component: HomeComponent },
-  // Aquí puedes agregar otras rutas según necesites
-  { path: '**', redirectTo: '' } // Redirección para rutas no encontradas
-];
